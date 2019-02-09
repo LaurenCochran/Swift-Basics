82981ca3f422472b5687089f9f115701108650bd
Lauren <Owner1@MacBooks-MacBook-Pro.local>
2019-02-08 18:33:45 -0600

    First Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Lauren on 2019-02-08.
+//  Copyright © 2019 Lauren Cochran. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
